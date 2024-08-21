32307242590d3674d16ac07fe6a8a35453e7be56
shayan.heidary <shayan.heidary@knime.com>
2024-08-21 10:04:15 +0200

    add  error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div>not-found</div>
+    <div>not-founded</div>
   )
 }
--- app/not-founded.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotFounded() {
-  return (
-    <div>not-founded</div>
-  )
-}
