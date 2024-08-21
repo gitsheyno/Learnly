f5225dd3f83de0aabc2111ab04d4566e28b36f64
shayan.heidary <shayan.heidary@knime.com>
2024-08-21 10:36:41 +0200

    check

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -1,9 +1,6 @@
 import NavDetail from "./NavDetails";
-import { getCurrentUser } from "@/utils/users";
 export default async function Nav() {
-  const user = await getCurrentUser();
+ 
 
-  const isAuthenticated = user ? true : false;
-
-  return <NavDetail isAuthenticated={isAuthenticated} />;
+  return <NavDetail isAuthenticated={true} />;
 }
