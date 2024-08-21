f1d17c241d3e27a7977b997dcf2b913da22a66ef
shayan.heidary <shayan.heidary@knime.com>
2024-08-21 10:13:21 +0200

    ttest

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,6 +1,7 @@
 import SigninForm from "@/app/ui/SigninForm";
 const SigninPage = () => {
-  return <SigninForm />;
+  // return <SigninForm />;
+  return <>hello</>;
 };
 
 export default SigninPage;
